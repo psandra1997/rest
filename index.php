853581f4777ded3e5af724105556e502dd75058c
Sandra Petkovski <sandrapetkovskifon@gmail.com>
2020-10-18 20:44:17 +0200

    promena localhost-a

--- index.php
+++ index.php
@@ -29,52 +29,52 @@
         <tbody>
         <tr>
             <td>GET</td>
-            <td><a href="localhost:8080/rest/api/novosti" target="_blank">localhost:8080/rest/novosti</a></td>
+            <td><a href="localhost:8080/rest/api/novosti" target="_blank">localhost:8080/rest/api/novosti</a></td>
             <td>Prikazuje sve novosti u json formatu <br></td>
         </tr>
         <tr>
             <td>GET</td>
-            <td><a href="localhost:8080/rest/api/novosti/@id" target="_blank">localhost:8080/rest/novosti/@id</a></td>
+            <td><a href="localhost:8080/rest/api/novosti/@id" target="_blank">localhost:8080/rest/api/novosti/@id</a></td>
             <td>prikazuje jednu novost za unesen @id</td>
         </tr>
         <tr>
             <td>GET</td>
-            <td><a href="localhost:8080/rest/api/kategorije" target="_blank">localhost:8080/rest/kategorije</a></td>
+            <td><a href="localhost:8080/rest/api/kategorije" target="_blank">localhost:8080/rest/api/kategorije</a></td>
             <td>Prikazuje sve kategorije u json formatu</td>
         </tr>
         <tr>
             <td>GET</td>
-            <td><a href="localhost:8080/rest/kategorije/@id" target="_blank">localhost:8080/rest/kategorije/@id</a></td>
+            <td><a href="localhost:8080/rest/api/kategorije/@id" target="_blank">localhost:8080/rest/api/kategorije/@id</a></td>
             <td>prikazuje jednu kategoriju za unesen @id</td>
         </tr>
         <tr>
             <td>POST</td>
-            <td>localhost:8080/rest/novosti</td>
+            <td>localhost:8080/rest/api/novosti</td>
             <td>Unosi jednu novost sa prosleđenim parametrima u json formatu u telu HTTP zahteva</td>
         </tr>
         <tr>
             <td>POST</td>
-            <td>localhost:8080/rest/kategorije</td>
+            <td>localhost:8080/rest/api/kategorije</td>
             <td>Unosi jednu kategoriju sa prosleđenim parametrima u json formatu u telu HTTP zahteva</td>
         </tr>
         <tr>
             <td>PUT</td>
-            <td>localhost:8080/rest/novosti/@id</td>
+            <td>localhost:8080/rest/api/novosti/@id</td>
             <td>Ažurira novost sa prosleđenim parametrima u json formatu u telu HTTP zahteva, čiji je id @id</td>
         </tr>
         <tr>
             <td>PUT</td>
-            <td>localhost:8080/rest/kategorije/@id</td>
+            <td>localhost:8080/rest/api/kategorije/@id</td>
             <td>Ažurira kategoriju sa prosleđenim parametrima u json formatu u telu HTTP zahteva, čiji je id @id</td>
         </tr>
         <tr>
             <td>DELETE</td>
-            <td>localhost:8080/rest/novosti/@id</td>
+            <td>localhost:8080/rest/api/novosti/@id</td>
             <td>Briše jednu novost čiji je id @id</td>
         </tr>
         <tr>
             <td>DELETE</td>
-            <td>localhost:8080/rest/kategorije/@id</td>
+            <td>localhost:8080/rest/api/kategorije/@id</td>
             <td>Briše jednu kategoriju čiji je id @id</td>
         </tr>
         </tbody>
